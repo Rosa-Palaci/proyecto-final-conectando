8b7ec4bb6db4dd535ae820f41eecd0ae2717f2b3
SebastianIL <a01661179@tec.mx>
2023-09-14 13:21:58 -0600

    Router en app

--- proyecto-semana-tec/src/App.js
+++ proyecto-semana-tec/src/App.js
@@ -4,20 +4,39 @@ import { Formulario } from "./components/Formulario";
 import { Tareas } from "./components/Tareas";
 import "./App.css";
 import React, { useState } from "react";
+import { createRoot } from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/calendario",
+    element: (
+      <Demo></Demo>
+    ),
+  },
+  {
+    path: "/tareas",
+    element: <Tareas></Tareas>
+  },
+  {
+    path: "/formulario",
+    element: <Formulario></Formulario>
+  },
+]);
+
 
 function App() {
-  const [tarea, setTarea] = React.useState({ nombre: "", link: "", date: "" });
-
   return (
     <div className="App">
-      <Demo></Demo>
-      <div className="gradient-bg">
-        {tarea.nombre !== "" ? (
-          <Tareas tarea={tarea} />
-        ) : (
-          <Formulario setTarea={setTarea} />
-        )}
-      </div>
+      
+      <React.StrictMode>
+        <RouterProvider router={router} />
+      </React.StrictMode>
     </div>
   );
 }
